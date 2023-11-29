c75fc17ed6921c3c5a0d42958031994246b61d80
Tamara Patterson <tamara_patterson@intuit.com>
2023-11-28 19:37:37 -0800

    add variables

---
+++ typescript-basics/project-files/1-getting-started/3-setting-up-a-project-begin/index.ts
@@ -0,0 +1,8 @@
+const name: string = 'Tamara';
+const msg: string = `Hello ${name}`;
+
+console.log(msg);
+
+
+// Exporting an empty object will treat this file as a module with its own scope
+export {};
