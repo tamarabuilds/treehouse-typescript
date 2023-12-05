a72c32057450677bb21c8aa4945de780d3801cf5
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-12-05 10:49:06 -0800

    complete getting started with typescript

--- typescript-basics/project-files/1-getting-started/4-basic-types-begin/basic-types.ts
+++ typescript-basics/project-files/1-getting-started/4-basic-types-begin/basic-types.ts
@@ -8,4 +8,26 @@ hasSuperPowers = Boolean('true');
 hasSuperPowers = !!'true';
 hasSuperPowers = !!'';
 
-console.log(hasSuperPowers);
+// console.log(hasSuperPowers);
+
+
+let heightInCm: number = 188;
+
+heightInCm = Number(true);
+heightInCm = Number('188');
+heightInCm = +('188');
+heightInCm = +(true);
+
+// console.log(heightInCm);
+
+
+let hero: string;
+
+hero = 'batman';
+hero = String(122);
+hero = String(true);
+hero = (122).toString();
+
+console.log(hero);
+
+
